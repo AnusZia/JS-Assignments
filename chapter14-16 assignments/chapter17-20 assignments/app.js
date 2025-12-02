0d2290e41f6d6ebeaecd090595a1ebc05ac57858
AnusZia <anusk8360@gmail.com>
2025-12-02 17:03:39 +0500

    Added new assignment files

---
+++ chapter14-16 assignments/chapter17-20 assignments/app.js
@@ -0,0 +1,17 @@
+// Q1 answer
+
+// var myArray = [];
+
+
+// //  Q2 answer
+
+// var matrix = [
+//     [0, 1,2 ,3], 
+//     [1, 0, 1 ,2],
+//     [2, 1, 0,1]
+// ];
+
+// Q3 answer
+for (  var i = 1 ; 1 <=10 ; i++){
+    console.log(i)
+} 
