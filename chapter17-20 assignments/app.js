24078617efa682fefc15b0d328c82cbe31074331
AnusZia <154527377+AnusZia@users.noreply.github.com>
2025-12-02 17:05:09 +0500

    Update app.js

--- chapter17-20 assignments/app.js
+++ chapter17-20 assignments/app.js
@@ -1,17 +1,106 @@
-// Q1 answer
+// Q1 Answer 
+var box1 = [[], []];
 
-// var myArray = [];
+
+// Q2 Answer 
+var box2 = [
+    [0, 1, 2, 3 + "<br>"] +
+    [1, 0, 1, 2 + "<br>"] +
+    [2, 1, 0, 1 + "<hr>"]
+];
+document.write(box2);
+
+// Q3 Answer 
+for (var num = 0; num <= 10; num++) {
+    document.write(num);
+}
+document.write("<hr>");
+
+// Q4 Answer 
+var tNum = prompt("Enter table number:");
+var tLen = prompt("Enter table length:");
+document.write("<hr>");
+
+for (var x = 1; x <= tLen; x++) {
+    document.write(tNum + " x " + x + " = " + (tNum * x) + "<br>");
+}
+document.write("<hr>");
 
 
-// //  Q2 answer
+// Q5 Answer 
+var fruitList = ["apple", "banana", "mango", "cherry", "watermelon"];
 
-// var matrix = [
-//     [0, 1,2 ,3], 
-//     [1, 0, 1 ,2],
-//     [2, 1, 0,1]
-// ];
+for (var f = 0; f < fruitList.length; f++) {
+    document.write("Item at index " + f + " is " + fruitList[f] + "<br>");
+}
+document.write("<hr>");
 
-// Q3 answer
-for (  var i = 1 ; 1 <=10 ; i++){
-    console.log(i)
-} 
+
+// Q6 Answer 
+var nums1 = [1,2,3,4,5,6,7,8,9];
+document.write("Counting: ");
+for (var a = 0; a < nums1.length; a++) {
+    document.write(nums1[a] + ",");
+}
+
+var nums2 = [9,8,7,6,5,4,3,2,1];
+document.write("<br>Reverse: ");
+for (var b = 0; b < nums2.length; b++) {
+    document.write(nums2[b] + ",");
+}
+
+var evenNums = [0,2,4,6,8,10,12,14,16,18,20];
+document.write("<br>Even: ");
+for (var c = 0; c < evenNums.length; c++) {
+    document.write(evenNums[c] + ",");
+}
+
+var oddNums = [1,3,5,7,9,11,13,15,17,19];
+document.write("<br>Odd: ");
+for (var d = 0; d < oddNums.length; d++) {
+    document.write(oddNums[d] + ",");
+}
+
+var seriesList = [2,4,6,8,10,12,14,16,18,20];
+document.write("<br>Series: ");
+for (var e = 0; e < seriesList.length; e++) {
+    document.write(seriesList[e] + "k,");
+}
+document.write("<hr>");
+
+
+// Q7 Answer 
+var items = ["cake", "apple pie", "biscuits", "chips", "patties"];
+var ask = prompt("What do you want?");
+var itemFound = false;
+var indexNumber = 0;
+
+for (var i = 0; i < items.length; i++) {
+    if (ask == items[i]) {
+        itemFound = true;
+        indexNumber = i;
+        alert(ask + " is available at index " + indexNumber);
+    }
+}
+
+if (itemFound == false) {
+    alert("Sorry, " + ask + " is not available.");
+}
+
+
+
+// Q8 Answer 
+var myNumbers = [24, 53, 78, 91, 12];
+var biggest = Math.max(...myNumbers);
+document.write("Largest number: " + biggest + "<hr>");
+
+// Q9 Answer 
+var myNums2 = [24, 53, 78, 91, 12];
+var smallest = Math.min(...myNums2);
+document.write("Smallest number: " + smallest + "<hr>");
+
+// Q10 Answer
+
+for (var i = 5; i <= 100; i = i + 5) {
+    document.write(i + ", ");
+}
